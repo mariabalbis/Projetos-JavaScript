d538d8489f0b860491a2133cfdc57b0f0b23a22e
mariabalbis <162700638+mariabalbis@users.noreply.github.com>
2024-05-29 12:07:46 -0300

    Add files via upload

---
+++ primeiratvd.js
@@ -0,0 +1,20 @@
+let nomePessoa
+let sobrenomePessoa = 'Balbis'
+var idadePessoa = 28
+var alturaPessoa = 1.85
+var usuarioLogado = false
+let numeroUm, numeroDois
+
+console.log(idadePessoa)
+console.log(alturaPessoa)
+
+// Saida de dados
+alert('O seu sobrenome é '+ sobrenomePessoa)
+alert(idadePessoa)
+alert('Olá, mundo :D')
+
+numeroUm = Number(prompt('Digite um número: '))
+numeroDois = Number(prompt('Digite o segundo número: '))
+
+alert(numeroUm + numeroDois)
+
