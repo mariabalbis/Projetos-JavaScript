62503382f164fccc5f11ef9d02038f83690b9c70
mariabalbis <162700638+mariabalbis@users.noreply.github.com>
2024-06-26 12:20:06 -0300

    Add files via upload

---
+++ Javascript +Html/file.js
@@ -0,0 +1,30 @@
+let elementoInputUM = document.getElementById('inptUm')
+let elementoInputDois = document.getElementById('inptDois')
+
+
+
+function Soma(){
+
+    alert (Number(elementoInputUM.value) + Number(elementoInputDois.value))
+
+}
+
+function Subtração(){
+
+    alert (Number(elementoInputUM.value) - Number(elementoInputDois.value))
+
+}
+
+function Divisão(){
+
+    alert (Number(elementoInputUM.value) / Number(elementoInputDois.value))
+
+}
+
+function Multiplicação(){
+
+    alert (Number(elementoInputUM.value) * Number(elementoInputDois.value))
+
+}
+
+
